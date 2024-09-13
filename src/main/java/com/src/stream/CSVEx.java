a30089305e88d538f0d58c38d0c5d57304b602d4
Abhik <abhikgh.work@gmail.com>
2024-09-13 19:05:03 +0530

    update

--- src/main/java/com/src/stream/CSVEx.java
+++ src/main/java/com/src/stream/CSVEx.java
@@ -1,15 +1,18 @@
 package com.src.stream;
 
 import javax.xml.bind.JAXBException;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class CSVEx {
 
     public static void main(String[] args) throws IOException, JAXBException {
 
-        //Read csv
+        //Read csv - scanner
         try(Scanner scanner = new Scanner(new File("src/main/resources/file1.csv"))) {
             scanner.useDelimiter(",");
             while (scanner.hasNext()) {
@@ -20,6 +23,15 @@ public class CSVEx {
             }
         }catch (Exception e){
             e.printStackTrace();
+        }
+
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/abcNew.txt"))){
+            String line = bufferedReader.readLine();
+            while (Objects.nonNull(line)) {
+                System.out.println(line);
+                // read next line
+                line = bufferedReader.readLine();
+            }
         }
 
     }
